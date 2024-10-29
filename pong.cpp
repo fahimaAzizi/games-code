e4ffcf98b9b4f218d260ffbc96778ef0b1970088
fahimaAzizi <fahimaazizi397@gmail.com>
2024-10-29 17:32:39 +0500

    pong game

--- pong.cpp
+++ pong.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <time.h>
-#include<conio.h>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
 using namespace std;
 
 enum eDir { STOP = 0, LEFT = 1, UPLEFT = 2, DOWNLEFT = 3, RIGHT = 4, UPRIGHT = 5, DOWNRIGHT = 6 };
@@ -70,117 +71,125 @@ public:
         return o;
     }
 };
- class cPaddle
- {
-    private:
-    int x,y;
-    int originalX , originalY;
-    public:
-    cPaddle()
-    {
-        x = y = 0;
+
+class cPaddle {
+private:
+    int x, y;
+    int originalX, originalY;
+public:
+    cPaddle() : x(0), y(0) {}
+
+    cPaddle(int posX, int posY) {
+        originalX = posX;
+        originalY = posY;
+        x = posX;
+        y = posY;
     }
-    
-      cPaddle(int posX, int posY):cPaddle()
-      {
-          originalX =posX;
-          originalY =posY;
-          x =posX;
-          y = posY;
-      }
-     inline void Reset() {x =originalX;y = originalY;}
-     inline int getX() {return x;}
-     inline int getY() {return y;}
-     inline void MoveUp(){y--;}
-     inline void MoveDown() {y++;}
-     friend ostream & operator << (ostream & o, cPaddle c)
-     {
-        o << "paddle [" << c.x << "," << c.y <<"]";
-        return o ;
-     }
- };
-    class cGameManger 
-    {
-    private:
-    int width ,height;
-    int score1 , score2;
-    char up1 , down1 , up2 , down2:
+
+    inline void Reset() { x = originalX; y = originalY; }
+    inline int getX() { return x; }
+    inline int getY() { return y; }
+    inline void MoveUp() { y--; }
+    inline void MoveDown() { y++; }
+
+    friend ostream& operator<<(ostream& o, const cPaddle& c) {
+        o << "paddle [" << c.x << "," << c.y << "]";
+        return o;
+    }
+};
+
+class cGameManger {
+private:
+    int width, height;
+    int score1, score2;
+    char up1, down1, up2, down2;
     bool quit;
     cBall* ball;
     cPaddle* player1;
-    cPaddle * player2;
-    public:
-     cGameManger(int w ,int h)
-    {
-       srend(time(NULL));
-       quit = false;
-       up1= 'w' ; up2 = 'i';
-       down1= 's'; down2 = 'k';
-       score1 = score2 = 0;
-       width = w ; height =h;
-       ball = new cBall(w / 2, h/2 );
-       player1= new cPaddle(1,h /2-3);
-       player2 = new cPaddle( w - 2 , h/ 2 - 3 );
+    cPaddle* player2;
+    string ballColor;  // Store current color of the ball
+    const string colors[6] = {"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"}; // Red, Green, Yellow, Blue, Magenta, Cyan
+
+public:
+    cGameManger(int w, int h) {
+        srand(time(NULL));
+        quit = false;
+        up1 = 'w'; up2 = 'i';
+        down1 = 's'; down2 = 'k';
+        score1 = score2 = 0;
+        width = w; height = h;
+        ball = new cBall(w / 2, h / 2);
+        player1 = new cPaddle(1, h / 2 - 3);
+        player2 = new cPaddle(w - 2, h / 2 - 3);
+        ballColor = colors[0]; // Initialize with the first color
     }
-    ~cGameManger()
-    {
-        delete ball , player1 ,player2
-    }
-    void scoreUp(cPaddle * player)
-    {
+
+
+    void scoreUp(cPaddle* player) {
         if (player == player1)
             score1++;
-        else if (player === player2)
-             score2++;
-             ball -> Reset();
-             player1 -> Reset();
-             player2 -> Reset();
+        else if (player == player2)
+            score2++;
+
+        ball->Reset();
+        player1->Reset();
+        player2->Reset();
     }
-    void Draw(){
-        system("clear");
-        for(int i=0; i <width +2 ; i++)
-          cout <<"\xB2";
-          cout <<endl ;
-          for (int i = 0; i < height+ 2; i++)
-          {
-           for (int j = 0; j < width; j++)
-           {
-            int ballx = ball ->getX();
-            int bally = ball -> getY();
-            int player1x =player1 -> getX();
-            int player2x = player2-> getX();
-            int player1y = player1-> getY();
-            int player2y = player2 ->getY();
-            if (j == 0)
-               cout<<"\xB2";
-            if (ballx == j && bally == i)
-              cout << "o";
-            else if (player1x == j && player1y == i)
-                cout << "\xDB";
-              else if (player2x == j && player2y == i)
-                cout << "\xDB";
-            else
-               cout <<" ";
 
-            if (j == width - 1 )
-            
-               cout << "\xB2";
+    void Draw() {
+    system("clear");
+    cout << "\033[42m"; // Set green background for the entire screen
 
+    for (int i = 0; i < width + 2; i++)
+        cout << "\xB2";
+    cout << endl;
 
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width + 2; j++) {
+            int ballx = ball->getX();
+            int bally = ball->getY();
+            int player1x = player1->getX();
+            int player2x = player2->getX();
+            int player1y = player1->getY();
+            int player2y = player2->getY();
 
-           }
-           
-          }
-          
-          for(int i=0; i <width +2 ; i++)
-          cout <<"#";
+            if (j == 0 || j == width + 1) {
+                cout << "\xB2";
+            } else if (ballx == j && bally == i) {
+                cout << ballColor << "o\033[42m"; // Use the current ball color
+            } else if (player1x == j && (player1y <= i && i < player1y + 4)) {
+                cout << "\033[31m\xDB\033[42m"; // Red color for player1 paddle
+            } else if (player2x == j && (player2y <= i && i < player2y + 4)) {
+                cout << "\033[30m\xDB\033[42m"; // Black color for player2 paddle
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
     }
-    } ;
- 
-int main() 
-{
-   cGameManger c(40,20);
-   c.Draw()
 
+    for (int i = 0; i < width + 2; i++)
+        cout << "\xB2";
+    cout << "\033[0m" << endl; // Reset colors to default
+    cout << "Score 1: " << score1 << "   Score 2: " << score2 << endl;
+}
+
+
+
+
+     
+
+    void Run() {
+        while (!quit) {
+            Draw();
+            Input();
+            Logic();
+        }
+    }
+};
+
+int main() {
+    cGameManger c(40, 20);
+    c.Run();
     return 0;
 }
